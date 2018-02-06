1776669b6e4817da5eec45b539992e38179c0d47
Bill Chirico <billchirico@gmail.com>
2018-02-06 01:38:48 -0500

    Initial project structure

---
+++ src/BapesTwitterBot.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BapesTwitterBot.Console
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
