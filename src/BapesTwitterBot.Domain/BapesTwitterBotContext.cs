1a64d34478820e91d9396bb5f5e71d699f7cf3f2
Bill Chirico <billchirico@gmail.com>
2018-02-10 00:20:06 -0500

    Omit connection string

--- src/BapesTwitterBot.Domain/BapesTwitterBotContext.cs
+++ src/BapesTwitterBot.Domain/BapesTwitterBotContext.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using BapesTwitterBot.Domain.Models;
+﻿using BapesTwitterBot.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BapesTwitterBot.Domain
 {
     public class BapesTwitterBotContext : DbContext
     {
+        private const string ConnectionString = "";
+        
         public DbSet<TwitterUser> TwitterUsers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=tcp:bapestwitterbot.database.windows.net,1433;Initial Catalog=BapesTwitterBot;Persist Security Info=False;User ID=bapes;Password=Hello123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer(ConnectionString);
         }
     }
 }
